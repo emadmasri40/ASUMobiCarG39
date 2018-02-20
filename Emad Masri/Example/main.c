1233affca20949fc12fb58803fb0579f4e2c77b3
Emadmasri <36333914+emadmasri40@users.noreply.github.com>
2018-02-20 20:47:38 +0200

    motor
    how connect Atmega32 with motor driver (L293D)

---
+++ Emad Masri/Example/main.c
@@ -0,0 +1,56 @@
+/*
+ * moto.c
+ *
+ * Created: 20-Feb-18 7:18:25 PM
+ * Author : Emad
+ */ 
+#define F_CPU 1000000UL
+#include <avr/io.h>
+#include <util/delay.h>
+
+
+int main(void)
+{
+	
+	DDRA=(0<<DDD1)|(0<<DDD0);
+	PORTA=(0<<PA0);
+	DDRB=(1<<DDB7) | (0<<DDB6) | (0<<DDB5) | (0<<DDB4) | (1<<DDB3) | (0<<DDB2) | (0<<DDB1) | (0<<DDB0);
+	// State: Bit7=T Bit6=T Bit5=T Bit4=T Bit3=0 Bit2=T Bit1=T Bit0=T
+	PORTB=(0<<PORTB7) | (0<<PORTB6) | (0<<PORTB5) | (0<<PORTB4) | (0<<PORTB3) | (0<<PORTB2) | (0<<PORTB1) | (0<<PORTB0);
+
+	// Port C initialization
+	// Function: Bit7=In Bit6=In Bit5=In Bit4=In Bit3=In Bit2=In Bit1=In Bit0=In
+	DDRC=(0<<DDC7) | (0<<DDC6) | (0<<DDC5) | (0<<DDC4) | (0<<DDC3) | (0<<DDC2) | (0<<DDC1) | (0<<DDC0);
+	// State: Bit7=T Bit6=T Bit5=T Bit4=T Bit3=T Bit2=T Bit1=T Bit0=T
+	PORTC= (0<<PORTC6) | (0<<PORTC5) | (0<<PORTC4) | (0<<PORTC3) | (0<<PORTC2) | (0<<PORTC1) | (0<<PORTC0);
+
+	// Port D initialization
+	// Function: Bit7=In Bit6=In Bit5=In Bit4=In Bit3=In Bit2=In Bit1=In Bit0=In
+	DDRD=(0<<DDD7) | (0<<DDD6) | (0<<DDD5) | (0<<DDD4) | (0<<DDD3) | (0<<DDD2) | (0<<DDD1) | (0<<DDD0);
+	// State: Bit7=T Bit6=T Bit5=T Bit4=T Bit3=T Bit2=T Bit1=T Bit0=T
+	PORTD=(0<<PORTD7) | (0<<PORTD6) | (0<<PORTD5) | (0<<PORTD4) | (0<<PORTD3) | (0<<PORTD2) | (0<<PORTD1) | (0<<PORTD0);
+
+	// Timer/Counter 0 initialization
+	// Clock source: T0 pin Rising Edge
+	// Mode: Fast PWM top=0xFF
+	// OC0 output: Inverted PWM
+	TCCR0=(1<<WGM00) | (1<<COM01) | (1<<COM00) | (1<<WGM01) | (0<<CS02) | (0<<CS01) | (1<<CS00);
+	TCNT0=0x00;
+	OCR0=0x00;
+
+	// Timer(s)/Counter(s) Interrupt(s) initialization
+	TIMSK=(0<<OCIE2) | (0<<TOIE2) | (0<<TICIE1) | (0<<OCIE1A) | (0<<OCIE1B) | (0<<TOIE1) | (0<<OCIE0) | (0<<TOIE0);
+PORTB=(0<<PORTB0);
+	while (1)
+	{
+	
+		OCR0=0x00;
+		if (PINA==(1<<PINA0))
+		{
+			PORTB=(1<<PORTB7);
+			OCR0=0xAB;
+			_delay_ms(3200);
+		}
+		
+	}
+}
