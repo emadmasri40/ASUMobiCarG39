39b059c2b29686ddf8a38eb484aa2b19a9658944
NadaHamdy98 <36543116+nadahamdy98@users.noreply.github.com>
2018-02-22 23:10:01 +0200

    Add files via upload

---
+++ nada/four_led.ino
@@ -0,0 +1,22 @@
+int counter;
+
+void setup()
+{
+  pinMode(4, OUTPUT);
+   pinMode(5, OUTPUT);
+   pinMode(6, OUTPUT);
+   pinMode(7, OUTPUT);
+
+}
+
+void loop()
+{
+for(int c=4;c<8;c++)
+ {
+ digitalWrite(c, HIGH);
+ delay(1000);
+ digitalWrite(c, LOW);
+ delay(1000);
+
+ }
+}
