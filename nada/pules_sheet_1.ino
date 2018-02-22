e29a4cb678f9019c3dc0bc39de381c2a432ad4d9
NadaHamdy98 <36543116+nadahamdy98@users.noreply.github.com>
2018-02-22 17:59:10 +0200

    Add files via upload

---
+++ nada/pules_sheet_1.ino
@@ -0,0 +1,13 @@
+
+int led = 9;
+void setup() {
+pinMode(led,OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+digitalWrite(led,HIGH);
+delay(500);
+digitalWrite(led,Low);
+delay(1500);
+}
