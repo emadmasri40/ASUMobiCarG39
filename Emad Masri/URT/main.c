c27e408f44a817846327a1f71c4daf71525f4f20
Emadmasri <36333914+emadmasri40@users.noreply.github.com>
2018-02-23 00:26:49 +0200

    URT
    UART stands for Universal Asynchronous Receiver Transmitter, whereas USART stands for Universal Synchronous Asynchronous Receiver Transmitter. They are basically just a piece of computer hardware that converts parallel data into serial data. The only difference between them is that UART supports only asynchronous mode, whereas USART supports both asynchronous and synchronous modes. Unlike Ethernet, Firewire etc., there is no specific port for UART/USART. They are commonly used in conjugation with protocols like RS-232, RS-434 etc. (we have specific ports for these two!).
    
    In synchronous transmission, the clock data is recovered separately from the data stream and no start/stop bits are used. This improves the efficiency of transmission on suitable channels since more of the bits sent are usable data and not character framing.
    
    The USART has the following components:
    
        A clock generator, usually a multiple of the bit rate to allow sampling in the middle of a bit period
        Input and output shift registers
        Transmit/receive control
        Read/write control logic
        Transmit/receive buffers (optional)
        Parallel data bus buffer (optional)
        First-in, first-out (FIFO) buffer memory (optional)
    ## USART Pin Configuration
    
    Now lets have a look at the hardware pins related to USART. The USART of the AVR occupies three hardware pins pins:
    
        RxD: USART Receiver Pin (ATMega8 Pin 2; ATMega16/32 Pin 14)
        TxD: USART Transmit Pin (ATMega8 Pin 3; ATMega16/32 Pin 15)
        XCK: USART Clock Pin (ATMega8 Pin 6; ATMega16/32 Pin 1)
    #HIT
    This code is being edited

---
+++ Emad Masri/URT/main.c
@@ -0,0 +1,84 @@
+/*
+ * task.c
+ *
+ * Created: 21-Feb-18 11:34:42 PM
+ * Author : Emad
+ */ 
+#define F_CPU 16000000UL
+#include <avr/io.h>
+#include <util/delay.h>
+
+
+int main(void)
+{
+   
+DDRA=(0<<DDA7) | (0<<DDA6) | (0<<DDA5) | (0<<DDA4) | (0<<DDA3) | (0<<DDA2) | (0<<DDA1) | (0<<DDA0);
+// State: Bit7=T Bit6=T Bit5=T Bit4=T Bit3=T Bit2=T Bit1=T Bit0=T
+PORTA=(0<<PORTA7) | (0<<PORTA6) | (0<<PORTA5) | (0<<PORTA4) | (0<<PORTA3) | (0<<PORTA2) | (0<<PORTA1) | (0<<PORTA0);
+
+// Port B initialization
+// Function: Bit7=In Bit6=In Bit5=In Bit4=In Bit3=In Bit2=In Bit1=In Bit0=In
+DDRB=(0<<DDB7) | (0<<DDB6) | (0<<DDB5) | (0<<DDB4) | (0<<DDB3) | (0<<DDB2) | (0<<DDB1) | (0<<DDB0);
+// State: Bit7=T Bit6=T Bit5=T Bit4=T Bit3=T Bit2=T Bit1=T Bit0=T
+PORTB=(0<<PORTB7) | (0<<PORTB6) | (0<<PORTB5) | (0<<PORTB4) | (0<<PORTB3) | (0<<PORTB2) | (0<<PORTB1) | (0<<PORTB0);
+
+// Port C initialization
+// Function: Bit7=In Bit6=In Bit5=In Bit4=In Bit3=In Bit2=In Bit1=In Bit0=In
+DDRC=(0<<DDC7) | (0<<DDC6) | (0<<DDC5) | (0<<DDC4) | (0<<DDC3) | (0<<DDC2) | (0<<DDC1) | (0<<DDC0);
+// State: Bit7=T Bit6=T Bit5=T Bit4=T Bit3=T Bit2=T Bit1=T Bit0=T
+PORTC=(0<<PORTC7) | (0<<PORTC6) | (0<<PORTC5) | (0<<PORTC4) | (0<<PORTC3) | (0<<PORTC2) | (0<<PORTC1) | (0<<PORTC0);
+
+// Port D initialization
+// Function: Bit7=In Bit6=In Bit5=In Bit4=In Bit3=In Bit2=In Bit1=In Bit0=In
+DDRD=(0<<DDD7) | (0<<DDD6) | (0<<DDD5) | (0<<DDD4) | (0<<DDD3) | (0<<DDD2) | (0<<DDD1) | (0<<DDD0);
+// State: Bit7=T Bit6=T Bit5=T Bit4=T Bit3=T Bit2=T Bit1=T Bit0=T
+PORTD=(0<<PORTD7) | (0<<PORTD6) | (0<<PORTD5) | (0<<PORTD4) | (0<<PORTD3) | (0<<PORTD2) | (0<<PORTD1) | (0<<PORTD0);
+
+/// USART initialization
+// Communication Parameters: 8 Data, 1 Stop, No Parity
+// USART Receiver: On
+// USART Transmitter: On
+// USART Mode: Asynchronous
+// USART Baud Rate: 9600
+UCSRA=(0<<RXC) | (0<<TXC) | (0<<UDRE) | (0<<FE) | (0<<DOR) | (0<<PE) | (0<<U2X) | (0<<MPCM);
+UCSRB=(0<<RXCIE) | (0<<TXCIE) | (0<<UDRIE) | (1<<RXEN) | (1<<TXEN) | (0<<UCSZ2) | (0<<RXB8) | (0<<TXB8);
+UCSRC=(1<<URSEL) | (0<<UMSEL) | (0<<UPM1) | (0<<UPM0) | (0<<USBS) | (1<<UCSZ1) | (3<<UCSZ0) | (0<<UCPOL);
+uint16_t UBRR_Value = 103;
+
+UBRRL = (uint8_t) UBRR_Value;
+
+UBRRH = (uint8_t) (UBRR_Value >> 8);
+unsigned char uart_recieve (void)
+{
+	while(~(UCSRA) & (1<<RXC));                   // wait while data is being received
+	return UDR;                                   // return 8-bit data
+}
+
+//char data;
+void UART_send_char(char data)
+{
+	
+	while(!(UCSRA & (1<<UDRE) ) );
+	
+	UDR = data;                            
+}
+void UART_send_string(char data0)
+{
+	
+	while( data0 >0)
+	UART_send_char( data0++);
+	UART_send_char('\0');}
+while (1)
+{
+	_delay_ms(20);
+	 char y;
+y='emad';
+	while (!( UCSRA & (1<<UDRE)));
+	UDR='2';
+	_delay_ms(250);
+	//v=uart_recieve();
+ UART_send_string(y);
+ _delay_ms(25000);
+}
+}
+
